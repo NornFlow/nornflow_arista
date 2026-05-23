ff54dcd0abd078ae9e79bd74290e0a80708b35df
André Lima <39955130+theandrelima@users.noreply.github.com>
2026-05-23 18:27:56 +0100

    feat(eos_api): export Pyeapi, CONNECTION_NAME, connect_kwargs_from_open and ensure_pyeapi_connection

--- nornflow_arista/eos_api/__init__.py
+++ nornflow_arista/eos_api/__init__.py
@@ -1,10 +1,22 @@
 """Arista EOS eAPI helpers for Nornir: map inventory to 'pyeapi' connections."""
 
-from nornflow_arista.eos_api.connect import connect_kwargs_from_host, node_from_host
+from nornflow_arista.eos_api.connect import (
+    connect_kwargs_from_host,
+    connect_kwargs_from_open,
+    ensure_pyeapi_connection,
+    node_from_host,
+)
+from nornflow_arista.eos_api.constants import PYEAPI_CONNECTION_NAME
 from nornflow_arista.eos_api.exceptions import EapiConfigError
+from nornflow_arista.eos_api.plugin import CONNECTION_NAME, Pyeapi
 
 __all__ = [
+    "CONNECTION_NAME",
     "EapiConfigError",
+    "PYEAPI_CONNECTION_NAME",
+    "Pyeapi",
     "connect_kwargs_from_host",
+    "connect_kwargs_from_open",
+    "ensure_pyeapi_connection",
     "node_from_host",
 ]
